2b9de513bf065eb045443daba4135e44f6d976df
smnahidweb <smnahidh040@gmail.com>
2025-05-20 20:32:25 +0600

    implented Firbase authentication security keys

--- src/Firbase/firebase.init.js
+++ src/Firbase/firebase.init.js
@@ -1,18 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDTw5MqS_7OFzPf_u6Zxkzq2Uyh5lG-DGQ",
-  authDomain: "tree-plant-store.firebaseapp.com",
-  projectId: "tree-plant-store",
-  storageBucket: "tree-plant-store.firebasestorage.app",
-  messagingSenderId: "981386914011",
-  appId: "1:981386914011:web:8a14014252c72bc823c4fe"
+   apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
